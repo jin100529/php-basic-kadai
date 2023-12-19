24ca4951dd911ad488aaa2ced352ae691bf68827
jin100529 <152959123+jin100529@users.noreply.github.com>
2023-12-19 14:02:28 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編課題</title>
+ </head>
+
+ <body>
+    <p>
+        <?php
+          $Product_data = ['name' => 'onion' , 'price' => 200 , 'weight' => 160];
+
+          print_r($Product_data);
+        ?>
+ </body>
+ </html>
